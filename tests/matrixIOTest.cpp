d916ec968362e847fbd0830365a81cd64d8f4da3
Mark Ashraf <markashraf@Marks-Laptop.local>
2023-02-08 13:45:08 +0100

    Modified name of test in the cmakelists file

--- tests/matrixIOTest.cpp
+++ tests/matrixIOTest.cpp
@@ -5,17 +5,23 @@
 
 using namespace Eigen;
 
-
-
-BOOST_FIXTURE_TEST_SUITE(matrixIOTests)
-
-BOOST_AUTO_TEST_CASE(matrixIOTest)
-{
-  MatrixXd outputMatrix = matrixIO::openData("../data/m3.csv", 3);
-  expected = MatrixXd(3, 3);
+struct matrixIOFixture {
+  matrixIOFixture()
+  {
+    expected = MatrixXd(3, 3);
     expected << 0.680375, 0.59688, -0.329554, -0.211234, 0.823295, 0.536459,
         0.566198, -0.604897, -0.444451;
-    
+  }
+
+  MatrixXd expected;
+};
+
+BOOST_FIXTURE_TEST_SUITE(matrixIOTests, matrixIOFixture)
+
+BOOST_AUTO_TEST_CASE(matrixIOTests)
+{
+  MatrixXd outputMatrix = matrixIO::openData("../data/m3.csv", 3);
+
   BOOST_TEST(expected(0, 0) == outputMatrix(0, 0));
   BOOST_TEST(expected(0, 1) == outputMatrix(0, 1));
   BOOST_TEST(expected(0, 2) == outputMatrix(0, 2));
