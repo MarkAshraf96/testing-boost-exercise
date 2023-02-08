5ba7f232406e20241f8ea791d1bf206214b1d2b2
Mark Ashraf <markashraf@Marks-Laptop.local>
2023-02-08 13:29:34 +0100

    Modified Test

--- tests/matrixIOTest.cpp
+++ tests/matrixIOTest.cpp
@@ -9,8 +9,9 @@ struct matrixIOFixture {
   matrixIOFixture()
   {
     expected = MatrixXd(3, 3);
-    expected << 0.680375, 0.59688, -0.329554, -0.211234, 0.823295, 0.536459,
-        0.566198, -0.604897, -0.444451;
+    expected << 0.680375, 0.59688, -0.329554,
+            -0.211234, 0.823295, 0.536459,
+            0.566198, -0.604897, -0.444451;
   }
 
   MatrixXd expected;
@@ -18,19 +19,19 @@ struct matrixIOFixture {
 
 BOOST_FIXTURE_TEST_SUITE(matrixIOTests, matrixIOFixture)
 
-BOOST_AUTO_TEST_CASE(matrixIOTests)
+BOOST_AUTO_TEST_CASE(OneTest)
 {
   MatrixXd outputMatrix = matrixIO::openData("../data/m3.csv", 3);
 
-  BOOST_TEST(expected(0) == outputMatrix(0));
-  BOOST_TEST(expected(1) == outputMatrix(1));
-  BOOST_TEST(expected(2) == outputMatrix(2));
-  BOOST_TEST(expected(3) == outputMatrix(3));
-  BOOST_TEST(expected(4) == outputMatrix(4));
-  BOOST_TEST(expected(5) == outputMatrix(5));
-  BOOST_TEST(expected(6) == outputMatrix(6));
-  BOOST_TEST(expected(7) == outputMatrix(7));
-  BOOST_TEST(expected(8) == outputMatrix(8));
+  BOOST_TEST(expected(0, 0) == outputMatrix(0, 0));
+  BOOST_TEST(expected(0, 1) == outputMatrix(0, 1));
+  BOOST_TEST(expected(0, 2) == outputMatrix(0, 2));
+  BOOST_TEST(expected(1, 0) == outputMatrix(1, 0));
+  BOOST_TEST(expected(1, 1) == outputMatrix(1, 1));
+  BOOST_TEST(expected(1, 2) == outputMatrix(1, 2));
+  BOOST_TEST(expected(2, 0) == outputMatrix(2, 0));
+  BOOST_TEST(expected(2, 1) == outputMatrix(2, 1));
+  BOOST_TEST(expected(2, 2) == outputMatrix(2, 2));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
